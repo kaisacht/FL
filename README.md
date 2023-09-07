a45e6ba5f01af87c8fde08d3f30720354bbde82e
kaisacht <leanhvut1k27cht@gmail.com>
2023-09-07 19:32:51 +0700

    fix data iid and non iid

---
+++ README.md
@@ -0,0 +1,1 @@
+Project Biuld FL basic
---
+++ model/Net.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import math
+
+class CNN_MNIST(nn.Module):
+    def __init__(self):
+        super(CNN_MNIST, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3,3))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+        self.max_pool = nn.MaxPool2d(kernel_size=(2, 2))
+        self.drop1 = nn.Dropout2d(p=0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.drop2 = nn.Dropout2d(p=0.5)
+        self.fc2 = nn.Linear(128, 10)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.max_pool(x)
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = self.drop1(x)
+        x = F.relu(self.fc1(x))
+        x = self.drop2(x)
+        x = self.fc2(x)
+        return x  
