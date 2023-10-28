bd1e36657e789e05c077c4a31e04932227fcdbb6
kaisacht <leanhvut1k27cht@gmail.com>
2023-10-29 00:04:41 +0700

    result with defense

--- run.bat
+++
@@ -1,6 +0,0 @@
-python main.py --dataset mnist --iid 0 --defence our --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
-python main.py --dataset mnist --iid 0 --defence avg --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
-python main.py --dataset mnist --iid 0 --defence RLR --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
-python main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
-python main.py --dataset mnist --iid 0 --defence krum --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
-python main.py --dataset mnist --iid 0 --defence fltrust --frac 0.3 --malicious 0.15 --epochs 2 --long 2 --wide 2
--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
-python3 main.py --dataset mnist --iid 0 --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
-python3 main.py --dataset mnist --iid 0 --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
-python3 main.py --dataset mnist --iid 0 --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
-python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
-python3 main.py --dataset mnist --iid 0 --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
-python3 main.py --dataset mnist --iid 0 --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 2 --wide 2
+python3 main.py --dataset mnist --iid 0 --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
+python3 main.py --dataset mnist --iid 0 --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5
+python3 main.py --dataset mnist --iid 0 --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
+python3 main.py --dataset mnist --iid 0 --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5   
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
+python3 main.py --dataset mnist --iid 0 --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
+python3 main.py --dataset mnist --iid 0 --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 4 --wide 5 
+python3 main.py --dataset mnist --iid 0 --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba   
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
