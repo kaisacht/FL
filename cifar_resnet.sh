16cdbe82301ec4bd34c097e184321eb6b7a237ea
Le Dac Khanh <khanhld@dasvision.vn>
2023-11-02 06:49:32 +0700

    ket qua voi resnet

--- cifar_resnet.sh
+++ cifar_resnet.sh
@@ -1,24 +1,3 @@
-python3 main.py --dataset cifar --iid 0 --model resnet --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
-python3 main.py --dataset cifar --iid 0 --model resnet --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5
-python3 main.py --dataset cifar --iid 0 --model resnet --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
-python3 main.py --dataset cifar --iid 0 --model resnet --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5   
-python3 main.py --dataset cifar --iid 0 --model resnet --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
-python3 main.py --dataset cifar --iid 0 --model resnet --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 
-python3 main.py --dataset cifar --iid 0 --model resnet --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5
-python3 main.py --dataset cifar --iid 0 --model resnet --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5
-python3 main.py --dataset cifar --iid 0 --model resnet --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
-python3 main.py --dataset cifar --iid 0 --model resnet --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
-python3 main.py --dataset cifar --iid 0 --model resnet --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba   
-python3 main.py --dataset cifar --iid 0 --model resnet --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
-python3 main.py --dataset cifar --iid 0 --model resnet --defence krum --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
-python3 main.py --dataset cifar --iid 0 --model resnet --defence fltrust --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
-python3 main.py --dataset cifar --iid 0 --model resnet --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba
 python3 main.py --dataset cifar --iid 1 --model resnet --defence our --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
 python3 main.py --dataset cifar --iid 1 --model resnet --defence avg --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
 python3 main.py --dataset cifar --iid 1 --model resnet --defence RLR --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3
