a010ee3f273158333920b942f865516ff57b6428
kaisacht <leanhvut1k27cht@gmail.com>
2023-11-07 17:45:02 +0700

    thêm file vẽ kết quả

--- plot.py
+++ plot.py
@@ -32,6 +32,8 @@ for file in files:
                 for line in lines:
                     if line.startswith("    Aggregation Function:"):
                         defense = line.strip().split(": ")[1] +''
+                    elif line.startswith("    IID:"):
+                        iid = line.strip().split(": ")[1] +''
                     elif line.startswith("    Attack method:"):
                         attack_method = line.strip().split(": ")[1] +''
                     elif line.startswith("    Long attack:"):
@@ -43,46 +45,45 @@ for file in files:
                     elif line.startswith('loss_list='):
                         test_loss = [float(value) for value in line.strip().split("[")[1].split("]")[0].split(", ")]
                     elif line.startswith("backdoor_accuracy="):
-             
                         backdoor_accuracy = [float(value) for value in line.strip().split("[")[1].split("]")[0].split(", ")]
             size_line = 1.25
-            if attack_method == 'badnet' and long_attack == '3' and wide_attack == '3':
-                if defense == 'RLR' or defense == 'mr_duc':
-                    plt.subplot(321)
-                    plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+            if iid == '1':
+                if attack_method == 'badnet' and long_attack == '3' and wide_attack == '3':
+                    if defense == 'RLR' or defense == 'mr_duc':
+                        plt.subplot(321)
+                        plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
 
-                    plt.ylabel('main accuracy')
-                    plt.legend()
-                    plt.subplot(322)
-                    plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
-                    plt.ylabel('backdoor accuracy')
-                    plt.legend()
-            elif attack_method == 'badnet' and long_attack == '5' and wide_attack == '5':
-                if defense == 'RLR' or defense == 'mr_duc':
-                
-                    plt.subplot(323)
-                    plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
-                    plt.ylabel('main accuracy')
-                    plt.legend()
-                    plt.subplot(324)
-                    plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
-                    plt.ylabel('backdoor accuracy')
-                    plt.legend()
-            elif attack_method == 'dba' and long_attack == '5' and wide_attack == '5':
-                if defense == 'RLR' or defense == 'mr_duc':
-                
-                    plt.subplot(325)
-                    plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
-                    plt.ylabel('main accuracy')
-                    plt.legend()
-                    plt.subplot(326)
-                    plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
-                    plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
-                    plt.ylabel('backdoor accuracy')
-                    plt.legend()
-plt.savefig('../FL/save/result.pdf', format = 'pdf',bbox_inches='tight')
+                        plt.ylabel('main accuracy')
+                        plt.legend()
+                        plt.subplot(322)
+                        plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+                        plt.ylabel('backdoor accuracy')
+                        plt.legend()
+                elif attack_method == 'badnet' and long_attack == '5' and wide_attack == '5':
+                    if defense == 'RLR' or defense == 'mr_duc':
+                        plt.subplot(323)
+                        plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+                        plt.ylabel('main accuracy')
+                        plt.legend()
+                        plt.subplot(324)
+                        plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+                        plt.ylabel('backdoor accuracy')
+                        plt.legend()
+                elif attack_method == 'dba' and long_attack == '5' and wide_attack == '5':
+                    if defense == 'RLR' or defense == 'mr_duc':
+                    
+                        plt.subplot(325)
+                        plt.plot(main_task_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+                        plt.ylabel('main accuracy')
+                        plt.legend()
+                        plt.subplot(326)
+                        plt.plot(backdoor_accuracy, label = defense, linewidth = size_line)
+                        plt.xlabel(attack_method +' '+ long_attack +'x' + wide_attack)
+                        plt.ylabel('backdoor accuracy')
+                        plt.legend()
+plt.savefig('../FL/save/result_mrduc.pdf', format = 'pdf',bbox_inches='tight')
