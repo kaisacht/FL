46aca81fe62885badad39efb1a63bfb4874f86f9
kaisacht <leanhvut1k27cht@gmail.com>
2023-11-15 10:48:57 +0700

    add fmnist.sh

---
+++ fashion_mnist.sh
@@ -0,0 +1,18 @@
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.3
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.35
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.4
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.45
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.3
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.35
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.4
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.45
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.3
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.35
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.4
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.45
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.5
+python3 main.py --dataset fashion_mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --attack dba --threshold_reject 0.6
