c94d11a531db8e8913d1ef849aae66f473ffd0c4
Le Dac Khanh <khanhld@dasvision.vn>
2023-11-27 08:51:09 +0700

    fex down

--- threshold_down.sh
+++ threshold_down.sh
@@ -1,24 +1,24 @@
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.1
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.2
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1 --attack dba
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2 --attack dba
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.1
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.2
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1 --attack dba
-python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2 --attack dba
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.1
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.2
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1 --attack dba
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2 --attack dba
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.1
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.2
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1 --attack dba
-python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.15 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.1
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.2
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.1
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.2
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2 --attack dba
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.1
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.5 --threshold_down 0.2
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.1 --attack dba
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.5 --threshold_down 0.2 --attack dba
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.1
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 3 --wide 3 --threshold_reject 0.6 --threshold_down 0.2
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.1 --attack dba
+python3 main.py --dataset cifar --model resnet --iid 1 --defence mr_duc --frac 0.3 --malicious 0.16 --epochs 100 --long 5 --wide 5 --threshold_reject 0.6 --threshold_down 0.2 --attack dba
