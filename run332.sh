68f043b066789c331941473be0c043c4c546434b
kaisacht <leanhvut1k27cht@gmail.com>
2024-01-30 13:18:57 +0700

    ket qua khi attack 3x3

---
+++ run332.sh
@@ -0,0 +1,9 @@
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 0 --attack dba 
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence flame --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 15 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 0
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 5
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 15
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 0 --attack dba 
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 5 --attack dba
+python3 main.py --dataset mnist --iid 0 --defence zkp --frac 0.3 --malicious 0.15 --epochs 100 --long 3 --wide 3  --q_noniid 15 --attack dba
