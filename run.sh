d801ca4562fd10580d795cee221e325ec81dd585
kaisacht <leanhvut1k27cht@gmail.com>
2024-08-29 07:38:19 +0700

    new sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence DABA --malicious 0.25 --attack dba 
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence avg --malicious 0.25 --attack dba 
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence RLR --malicious 0.25 --attack dba 
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence flame --malicious 0.25 --attack dba 
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence krum --malicious 0.25 --attack dba 
-python3.10 main.py --dataset cifar --model resnet50 --iid 1 --defence fltrust --malicious 0.25 --attack dba 
 python3.10 main.py --dataset cifar --model resnet101 --iid 1 --defence DABA --malicious 0.25 --attack dba 
 python3.10 main.py --dataset cifar --model resnet101 --iid 1 --defence avg --malicious 0.25 --attack dba 
 python3.10 main.py --dataset cifar --model resnet101 --iid 1 --defence RLR --malicious 0.25 --attack dba 
